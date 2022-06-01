0064cdb9af503099352ccf31fdb661696b9c2543
Samoina <samoina.w@gmail.com>
2022-06-01 11:31:56 +0300

    Use template literals in the url for the API website, and add the user's input number in the placeholder

--- EXTERNAL API/app.js
+++ EXTERNAL API/app.js
@@ -1,14 +1,21 @@
 document.querySelector('.get-jokes').addEventListener('click', loadJokes);
 
-function loadJokes(ev){
+function loadJokes(ev) {
   const number = parseInt(document.querySelector('#number').value);
- 
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://api.icndb.com/jokes/random/<number>', true);
 
-  xhr.onload = function(){
-    const joke = JSON.parse(this.responseText);
-    console.log(joke.value);
+  //initialize a newly-created request
+  const xhr = new XMLHttpRequest();
+
+  // xhr.open('GET', 'http://api.icndb.com/jokes/random/<number>', true);
+
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
+  //what do we want to do with this data
+  xhr.onload = function () {
+    if (this.status === 200) {
+      const joke = JSON.parse(this.responseText);
+      console.log(joke);
+    }
   }
   xhr.send();
 
