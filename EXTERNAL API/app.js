76c7baa4fbf2efa7dc8f1f61510b0520acd019ef
Samoina <samoina.w@gmail.com>
2022-06-01 14:15:07 +0300

    USe callbacks to demonstrate both synchronous and asynchronous JS

--- EXTERNAL API/app.js
+++ EXTERNAL API/app.js
@@ -24,6 +24,8 @@ function loadJokes(ev) {
           listItem.appendChild(jokeItem);
           listJokes.appendChild(listItem);
         }
+      } else {
+        output += '<li>Something went wrong</li>'
       }
     }
   }
@@ -37,6 +39,6 @@ function loadJokes(ev) {
 i was making a mistake here because I put in a number, say 23, thinking it would bring the 23rd joke, but it is meant to bring 23 random jokes in an array form 
 
 NOTE 2
-the xhr.onload is for what we want to do with the information we get from the website. The idea was to display the jokes on the website, so i created a variable for the ul on the html page. the jokes are stored in array form in joke.value (jokeArr). this is what I will loop over using a for-loop. i created a list element, and a text node with the specific joke. I then appended the textnode onto the list item, and the list item onto the ul
-
+the xhr.onload is for what we want to do with the information we get from the website. The idea was to display the jokes on the website, so i created a variable for the ul on the html page. the jokes are stored in array form in joke.value (jokeArr). Because the respons eis an objevct, so we need to get its value. APIs are formatted differently, some will return an arrayt so it's easy to loop through. this is what I will loop over using a for-loop. but first make sure that the type is success. i created a list element, and a text node with the specific joke. I then appended the textnode onto the list item, and the list item onto the ul
+//some APIs require authentication 
 */
