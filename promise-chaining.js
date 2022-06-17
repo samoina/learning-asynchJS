9df09dc01e95068252904d9c6ad34b0bf8eab426
Samoina <samoina.w@gmail.com>
2022-06-17 15:46:47 +0300

    Decided to draft notes for a new blogpost starting from synchronous to asynchronous JS

---
+++ promise-chaining.js
@@ -0,0 +1,15 @@
+/*
+PROMISE CHAINING
+this is practice for promises and promise chaining with what I have learnt so far in Async Javascript.
+Promises are a part of Asynchronous Javascript. But what exactly is Asynchronous Javascript? or synchronous Javascript?
+
+Synchronous Javascript refers to the way in which JS runs code from the top to the bottom. The code below is synchronous, and it will log a, b, c in that order
+*/
+
+console.log('a');
+console.log('b');
+console.log('c');
+
+/*
+
+*/
